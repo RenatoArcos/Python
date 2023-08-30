aa8860325214258ac710caf245a239a0bc7b50ca
Renato Arcos <116533710+RenatoArcos@users.noreply.github.com>
2023-08-29 18:07:30 -0600

    Add files via upload

---
+++ Print Calendar.py
@@ -0,0 +1,10 @@
+from calendar import*
+year = int(input("Enter year:  "))
+print(calendar(year, 2, 1, 8, 3))
+
+# 2 = 2 characters for days (Mo, Tu, etc...)
+# 1 = 1 line (row for each week)
+# 8 = 8 rows for each month
+# 3 = = columns for all months of the year
+
+# by clcoding.com
